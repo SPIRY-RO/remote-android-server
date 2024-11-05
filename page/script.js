e3ab64ec4e175d92249a705d70a0ad88cc258e0f
Spiry <spirymedia@gmail.com>
2024-11-05 02:50:14 +0200

    Update script.js

--- page/script.js
+++ page/script.js
@@ -33,16 +33,8 @@ let dragThreshold = 5; // Set a threshold to determine if it's a drag or a click
 
 const config = {
     iceServers: [
-        { urls: "stun:stun.l.google.com:19302" },
-        { urls: "stun:stun.l.google.com:5349" },
-        { urls: "stun:stun1.l.google.com:3478" },
-        { urls: "stun:stun1.l.google.com:5349" },
-        { urls: "stun:stun2.l.google.com:19302" },
-        { urls: "stun:stun2.l.google.com:5349" },
-        { urls: "stun:stun3.l.google.com:3478" },
-        { urls: "stun:stun3.l.google.com:5349" },
-        { urls: "stun:stun4.l.google.com:19302" },
-        { urls: "stun:stun4.l.google.com:5349" },
+        { urls: "stun:stun.flashdance.cx:3478" },
+ 
         {
             urls: "turn:relay1.expressturn.com:80",
             username: "efKY1G0OOQ9YBWHHNE",
